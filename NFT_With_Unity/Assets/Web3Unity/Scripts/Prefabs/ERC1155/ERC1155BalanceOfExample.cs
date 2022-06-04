b1d4eb42d048a8a6217354a52fcd3df2cf88340b
hwankee <abcy100@nate.com>
2022-06-04 12:44:56 +0900

    WebGL Test version
    docs: https://docs.gaming.chainsafe.io/

--- NFT_With_Unity/Assets/Web3Unity/Scripts/Prefabs/ERC1155/ERC1155BalanceOfExample.cs
+++ NFT_With_Unity/Assets/Web3Unity/Scripts/Prefabs/ERC1155/ERC1155BalanceOfExample.cs
@@ -10,11 +10,11 @@ public class ERC1155BalanceOfExample : MonoBehaviour
     
     async void Start()
     {
-        string chain = "ethereum";
-        string network = "rinkeby";
-        string contract = "0x495f947276749Ce646f68AC8c248420045cb7b5e";
-        string account = PlayerPrefs.GetString("Account");
-        string tokenId = "83484095403942972080560959395318884959963213561662847513049032258614466707457";
+        string chain = "avalanche";
+        string network = "testnet";
+        string contract = "0xbDF2d708c6E4705824dC024187cd219da41C8c81";
+        string account = "0xdD4c825203f97984e7867F11eeCc813A036089D1";
+        string tokenId = "2";
 
         BigInteger balanceOf = await ERC1155.BalanceOf(chain, network, contract, account, tokenId);
         print(balanceOf);
