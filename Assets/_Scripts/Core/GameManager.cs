8f08256b46465192f1fe48d342e9528252b13e01
hwankee <abcy100@nate.com>
2022-06-17 00:33:33 +0900

    new main project

--- Assets/_Scripts/Core/GameManager.cs
+++ Assets/_Scripts/Core/GameManager.cs
@@ -1,9 +1,48 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
+using System.Threading;
 using UnityEngine;
 
 namespace Envy.Core
 {
+    class Counter
+    {
+        private const int LOOP_COUNT = 1000;
+
+        private readonly object thisLock;
+        private bool lockedCount = false;
+
+        private int count;
+
+        public int Count
+        {
+            get { return count; }
+        }
+
+        public Counter()
+        {
+            thisLock = new object();
+            count = 0;
+        }
+
+        public void Increase()
+        {
+            int loopCount = LOOP_COUNT;
+
+            while (loopCount-- > 0)
+            {
+                lock (thisLock)
+                {
+                    while (count > 0 || lockedCount == true)
+                    {
+                        Monitor.Wait(thisLock);
+                    }
+                    
+                    
+                }
+            }
+        }
+    }
+    
     public class GameManager : MonoBehaviour
     {
         void Start()
