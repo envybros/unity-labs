2e2da651325029c340e22b0967a3665c89254e75
hwankee <abcy100@nate.com>
2022-06-21 18:10:40 +0900

    Threading Test 2

--- Assets/_Scripts/Core/Managers/GameManager.cs
+++ Assets/_Scripts/Core/Managers/GameManager.cs
@@ -1,23 +1,47 @@
 using System;
-using UnityEngine;
+using System.Threading;
 using Cysharp.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.UI;
 
 namespace Envy.Main
 {
     public class GameManager : MonoBehaviour
     {
-        public async UniTaskVoid WhileTest()
+        [SerializeField] private Button cancelButton;
+        [SerializeField] private Button disposeButton;
+        private CancellationTokenSource _cancellationTokenSource;
+
+        private void Awake()
+        {
+            _cancellationTokenSource = new CancellationTokenSource();
+            cancelButton.onClick.AddListener(CancelButton);
+            disposeButton.onClick.AddListener(DisposeButton);
+        }
+
+        private async UniTaskVoid Start()
+        {
+            WhileTest();
+        }
+
+        private async UniTaskVoid WhileTest()
         {
             while (true)
             {
-                await UniTask.Yield(this.GetCancellationTokenOnDestroy());
+                await UniTask.Delay(TimeSpan.FromSeconds(0.5f),
+                    cancellationToken: _cancellationTokenSource.Token);
                 Funcs.WriteLine(Time.realtimeSinceStartup);
             }
         }
 
-        public async UniTask Start()
+        private void CancelButton()
         {
-            WhileTest();
+            _cancellationTokenSource.Cancel();
+        }
+
+        private void DisposeButton()
+        {
+            _cancellationTokenSource.Dispose();
         }
 
         private void OnDestroy()
