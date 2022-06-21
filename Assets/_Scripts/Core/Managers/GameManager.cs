879ba9e0f06d571029f74face8c2a7819aa0042a
hwankee <abcy100@nate.com>
2022-06-21 17:33:46 +0900

    import UniTask with package Manager And Test

--- Assets/_Scripts/Core/Managers/GameManager.cs
+++ Assets/_Scripts/Core/Managers/GameManager.cs
@@ -1,44 +1,28 @@
 using System;
 using UnityEngine;
 using Cysharp.Threading.Tasks;
-using UniTasks = Cysharp.Threading.Tasks.UniTask;
 
 namespace Envy.Main
 {
     public class GameManager : MonoBehaviour
     {
-        public async UniTaskVoid Function()
+        public async UniTaskVoid WhileTest()
         {
-            Funcs.WriteLine("Entrance");
-            await UniTasks.Delay(TimeSpan.FromSeconds(5f)); // 5초
-            Funcs.WriteLine(("Exit"));
+            while (true)
+            {
+                await UniTask.Yield(this.GetCancellationTokenOnDestroy());
+                Funcs.WriteLine(Time.realtimeSinceStartup);
+            }
         }
 
-        public async UniTask<float> FunctionFloat()
+        public async UniTask Start()
         {
-            Funcs.WriteLine("Float Entrance");
-            await UniTasks.Delay(TimeSpan.FromSeconds(5f)); // 5초
-
-            Funcs.WriteLine("Delay 100 Frame");
-            await UniTasks.DelayFrame(100); // 100 프레임
-
-            Funcs.WriteLine("Delay 1");
-            await UniTasks.DelayFrame(100); // 100 프레임
-
-            return 1.0f;
+            WhileTest();
         }
 
-        // 이것도 되나봄
-        public async UniTasks Start()
+        private void OnDestroy()
         {
-            Function().Forget(); // forget?
-            var f = await FunctionFloat();
-            Funcs.WriteLine($"Value => {f}");
-        }
-
-        public void Update()
-        {
-            
+            Funcs.WriteLine("삭제됨");
         }
     }
 }
